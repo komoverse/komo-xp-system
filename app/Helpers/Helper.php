71d3898e7b4dbc0eaa5c58c0ee773f7776587ae6
Karuna053 <karunayauw@gmail.com>
2023-03-03 09:42:12 +0700

    Validation in add_xp() now returns a proper error message rather than just breaking. Hid error messages if APP_ENV is set to 'production'.

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -6,6 +6,19 @@ class Helper
 {
     public static function is_https() {
         if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') return true;
+
         return isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on');
+    }
+
+    public static function is_local() {
+        if (env('APP_ENV') == 'local') return true;
+
+        return false;
+    }
+
+    public static function is_production() {
+        if (env('APP_ENV') == 'production') return true;
+
+        return false;
     }
 }
--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\DailyExperience;
 use App\Models\DailyExperienceEvent;
 use App\Models\MmrExperience;
@@ -14,63 +15,76 @@ use Carbon\Carbon;
 
 class ExperienceController extends Controller
 {
+    private $is_https;
+
+    public function __construct(){
+        $this->is_https = Helper::is_https();
+        $this->is_local = Helper::is_local();
+        $this->is_production = Helper::is_production();
+    }
+
     public function api_daily_experience_post(Request $request){
         // Verify connection is from HTTPS.
-        if (Helper::is_https() == false && env('APP_ENV') != 'local') {
-            return json_encode(array([
-                'status' => 'fail',
-                'message' => 'Connection fired from an unsecured connection (use HTTPS).',
-            ]));
+        if ($this->is_https == false && !$this->is_local == true) {
+            $json['status'] = 'fail';
+            $json['message'] = ($this->is_production == false) ? 'Connection fired from an unsecured connection (use HTTPS).' : null;
+
+            return response()->json($json, 400); // Bad Request
         }
 
         // List of API actions.
         if ($request['add-xp'] == 'true') {
-            return json_encode($this->add_xp($request));
+            return $this->add_xp($request);
         }
     }
-
-    // public function api_daily_experience_get(Request $request){
-    //     if ($request['get-front-page'] == 'true') {
-    //         return json_encode($this->get_frontpage());
-    //     }
-    // }
 
     /* ----- HELPER FUNCTIONS ----- */
 
     private function add_xp(Request $request){
-        // TODO: REWORK VALIDATION SO IT SENDS A PROPER ERROR MESSAGE
-        // TODO: Verify that API source and username exists in the DB, don't allow otherwise
-        $validated = $request->validate([
-            'komo-username' => 'required|max:255',
-            'amount' => 'required|numeric|max:2147483647',
-            'source' => 'required|max:255',
-        ]);
-
         // Initialize variables.
-        $daily_experience = $this->get_daily_experience($request);
         $komo_username = $request['komo-username'];
         $amount = $request['amount'];
         $source = $request['source'];
+        $json = [
+            'status' => 'fail',
+            'message' => null,
+        ];
 
-        // Start modifying current daily experience for user.
-        $daily_experience->total_experience += $amount;
-        if ($daily_experience->total_experience < 0 ) {
-            $daily_experience->total_experience = 0;
+        // Validate entry.
+        $validator = Validator::make($request->all(), [
+            'komo-username' => 'required|exists:tb_account,komo_username|max:255',
+            'amount' => 'required|numeric|max:2147483647',
+            'source' => 'required|exists:tb_api_key,source|max:255',
+            'api_key' => 'required|exists:tb_api_key,api_key|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            $json['message'] = ($this->is_production == false) ? $validator->errors() : null;
+            return response()->json($json, 400); // Bad Request
         }
 
+        // Start tallying up the experience gained.
+        $daily_experience = $this->get_daily_experience($request);
+        $daily_experience->total_experience += $amount;
+        max($daily_experience->total_experience, 0); // Sets any negative numbers to 0.
         $daily_experience->save();
 
-        // Create audit record of this EXP event.
+        // Create an event for audit purposes.
+        $daily_experience_event = $this->create_daily_experience_event($request, $daily_experience);
+
+        // Return API status.
+        $json['status'] = 'success';
+        $json['message'] = (!$this->is_production) ? 'Experience successfully added to account! Audit record has been created.': null;
+
+        return $json;
+    }
+
+    private function create_daily_experience_event(Request $request, DailyExperience $daily_experience) {
         $delta = $daily_experience->total_experience - $daily_experience->getOriginal('total_experience');
-        $daily_experience_event = DailyExperienceEvent::create([
+        return DailyExperienceEvent::create([
             'daily_experience_id' => $daily_experience->id,
             'source' => $source,
             'delta' => $delta,
-        ]);
-
-        return array([
-            'status' => 'success',
-            'message' => 'Experience successfully added to account! Audit record has been created.',
         ]);
     }
 
@@ -95,7 +109,7 @@ class ExperienceController extends Controller
         $daily_experience = DailyExperience::create([
             'komo_username' => $komo_username,
             'total_experience' => 0,
-        ]); 
+        ]);
 
         return $daily_experience;
     }
