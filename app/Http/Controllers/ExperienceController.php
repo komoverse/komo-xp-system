60cf0a85fe44c88052cf54d127726b9327324e4e
Karuna053 <karunayauw@gmail.com>
2023-03-03 14:26:52 +0700

    Implemented throttling system from Laravel + Code Maintenance.

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\RateLimiter;
 use App\Models\DailyExperience;
 use App\Models\DailyExperienceEvent;
 use App\Models\MmrExperience;
@@ -15,22 +16,27 @@ use Carbon\Carbon;
 
 class ExperienceController extends Controller
 {
-    private $is_https;
-
     public function __construct(){
         $this->is_https = Helper::is_https();
         $this->is_local = Helper::is_local();
         $this->is_production = Helper::is_production();
+
+        // Default JSON.
+        $this->json = array('status' => 'fail', 'message' => null);
     }
 
     public function api_daily_experience_post(Request $request){
-        // Verify connection is from HTTPS.
-        if ($this->is_https == false && !$this->is_local == true) {
-            $json['status'] = 'fail';
-            $json['message'] = ($this->is_production == false) ? 'Connection fired from an unsecured connection (use HTTPS).' : null;
+        // Verify connection is from HTTPS, but automatically passes if APP_ENV is local.
+        $connection_safe = $this->verify_connection($request);
+        if (!$connection_safe) return response()->json($this->json, 400); // Bad Request
 
-            return response()->json($json, 400); // Bad Request
-        }
+        // Verify that user exists in the main database.
+        $user_exists = $this->verify_user($request);
+        if (!$user_exists) return response()->json($this->json, 400); // Bad Request
+
+        // Throttle attempts to this API by 30 attempts/minute.
+        $rate_limited = $this->verify_rate_limit($request);
+        if ($rate_limited) return response()->json($this->json, 400); // Bad Request
 
         // List of API actions.
         if ($request['add-xp'] == 'true') {
@@ -41,12 +47,6 @@ class ExperienceController extends Controller
     /* ----- HELPER FUNCTIONS ----- */
 
     private function add_xp(Request $request){
-        // Initialize variables.
-        $json = [
-            'status' => 'fail',
-            'message' => null,
-        ];
-
         // Validate entry.
         $validator = Validator::make($request->all(), [
             'komo-username' => 'required|exists:tb_account,komo_username|max:255',
@@ -56,12 +56,12 @@ class ExperienceController extends Controller
         ]);
 
         if ($validator->fails()) {
-            $json['message'] = (!$this->is_production) ? $validator->errors() : null;
-            return response()->json($json, 400); // Bad Request
+            $this->json['message'] = (!$this->is_production) ? $validator->errors() : null;
+            return response()->json($this->json, 400); // Bad Request
         }
 
         // Start tallying up the experience gained.
-        $daily_experience = $this->get_daily_experience($request);
+        $daily_experience = $this->retrieve_daily_experience($request);
         $daily_experience->total_experience += max($daily_experience->total_experience + $request['amount'], 0);
         $daily_experience->save();
 
@@ -69,9 +69,9 @@ class ExperienceController extends Controller
         $daily_experience_event = $this->create_daily_experience_event($request, $daily_experience);
 
         // Return API status.
-        $json['status'] = 'success';
-        $json['message'] = (!$this->is_production) ? 'Experience successfully added to account! Audit record has been created.': null;
-        return $json;
+        $this->json['status'] = 'success';
+        $this->json['message'] = (!$this->is_production) ? 'Experience successfully added to account! Audit record has been created.': null;
+        return $this->json;
     }
 
     private function create_daily_experience_event(Request $request, DailyExperience $daily_experience) {
@@ -84,7 +84,7 @@ class ExperienceController extends Controller
         ]);
     }
 
-    private function get_daily_experience (Request $request) {
+    private function retrieve_daily_experience (Request $request) {
         $daily_experience = DailyExperience::where('komo_username', $request['komo-username'])
             ->whereDate('created_at', Carbon::today())
             ->orderBy('id', 'ASC')
@@ -104,5 +104,42 @@ class ExperienceController extends Controller
         ]);
 
         return $daily_experience;
+    }
+
+    private function verify_connection(Request $request){
+        if ($this->is_https == false && !$this->is_local == true) {
+            $this->json['message'] = (!$this->is_production) ? 'Connection fired from an unsecured connection (use HTTPS).' : null;
+            return false;
+        }
+
+        return true;
+    }
+
+    private function verify_user(Request $request){
+        $validator = Validator::make($request->all(), [
+            'komo-username' => 'required|exists:tb_account,komo_username|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            $this->json['message'] = (!$this->is_production) ? $validator->errors() : null;
+            return false;
+        }
+
+        return true;
+    }
+
+    private function verify_rate_limit(Request $request, $attempts_per_minute = 1){
+        $rate_limited = !RateLimiter::attempt(
+            'User: ' . $request['komo-username'],
+            $attempts_per_minute,
+            function() {}
+        );
+
+        if ($rate_limited) {
+            $this->json['message'] = (!$this->is_production) ? 'You are being rate limited.' : null;
+            return true;
+        }
+
+        return false;
     }
 }
