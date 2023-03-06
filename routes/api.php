92191a92e079fdce5a55af00b3b463a5b17620d1
Karuna053 <karunayauw@gmail.com>
2023-03-06 11:04:10 +0700

    Tweaking the api.php.

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/daily-experience', [App\Http\Controllers\ExperienceController::class, 'api_daily_experience_post']); 
-// Route::get('/daily-experience', [App\Http\Controllers\ExperienceController::class, 'api_daily_experience_get']);
+Route::get('/daily-experience', [App\Http\Controllers\ExperienceController::class, 'api_daily_experience_get']);
+Route::post('/daily-experience', [App\Http\Controllers\ExperienceController::class, 'api_daily_experience_post']);
